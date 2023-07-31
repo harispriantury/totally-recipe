7f77f868054aea21dbf575fb89cc20d04e98e411
HarisPriantury <prianturyharis@gmail.com>
2023-07-31 23:46:42 +0700

    change any type

--- src/store/search.ts
+++ src/store/search.ts
@@ -1,13 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "./store";
 
+export type TState = {
+  value: string;
+};
+
+type Action = {
+  type: string;
+  payload: string; // Di sini, payload memiliki tipe string karena initialState memiliki properti "value" dengan tipe string.
+};
+
 export const searchSlice = createSlice({
   name: "search",
   initialState: {
     value: ""
   },
   reducers: {
-    changeValue: (state, action) => {
+    changeValue: (state: TState, action: Action) => {
       state.value = action.payload;
     }
   }
